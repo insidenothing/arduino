92f3feba891794b729322fc8c1259ec9296dc1bf
Patrick McGuire <insidenothing@gmail.com>
2015-07-25 08:44:55 -0400

    (70%) of program storage (81%) of dynamic memory

--- wifi_sd_gps/wifi_sd_gps.ino
+++ wifi_sd_gps/wifi_sd_gps.ino
@@ -1,263 +1,56 @@
 #include <SPI.h>
-
-// WiFi
 #include <WiFi.h>
-
-// SDcard
-#include <SD.h>
-
-// The TinyGPS++ object
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
-static const int RXPin = 2, TXPin = 3;
-static const uint32_t GPSBaud = 9600;
+#include <SD.h>
 TinyGPSPlus gps;
-SoftwareSerial ss(RXPin, TXPin);
-
-
-
+SoftwareSerial ss(2, 3);
 File myFile;
-
 void setup() {
   Serial.begin(9600);
-  while(!Serial) ;
-  
-  
-  Serial.println("Initializing Wifi...");
-  printMacAddress();
-  ss.begin(GPSBaud);
-
-  Serial.print("Initializing SD card...");
-  if (!SD.begin(4)) {
-    Serial.println("initialization failed!");
-    return;
-  }
-  delay(5000);
-
-  Serial.println(F("FullExample.ino"));
-  Serial.println(F("An extensive example of many interesting TinyGPS++ features"));
-  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
-  Serial.println(F("by Mikal Hart"));
-  Serial.println();
-  Serial.println(F("Sats HDOP Latitude   Longitude   Fix  Date       Time     Date Alt    Course Speed Card  Distance Course Card  Chars Sentences Checksum"));
-  Serial.println(F("          (deg)      (deg)       Age                      Age  (m)    --- from GPS ----  ---- to London  ----  RX    RX        Fail"));
-  Serial.println(F("---------------------------------------------------------------------------------------------------------------------------------------"));
-  
-  // scan for existing networks:
-  delay(1000);
-  Serial.println("Setup completed");
+  ss.begin(9600);
+  SD.begin(4);
 }
-
 void loop() {
-  delay(1000);
-  Serial.println("** Begin loop() **");
-  delay(2000);
-  myFile = SD.open("data.txt", FILE_WRITE);
-  delay(2000);
-  if (myFile) {
-    Serial.println("SD Card: Open");  
-  } else {
-    Serial.println("SD Card: Error");
-  }
-  delay(1000);
-  Serial.println("SD Card: Ready");
-  static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
-  printInt(gps.satellites.value(), gps.satellites.isValid(), 5);
-  printInt(gps.hdop.value(), gps.hdop.isValid(), 5);
-  printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
-  printFloat(gps.location.lng(), gps.location.isValid(), 12, 6);
-  printInt(gps.location.age(), gps.location.isValid(), 5);
-  printDateTime(gps.date, gps.time);
-  printFloat(gps.altitude.meters(), gps.altitude.isValid(), 7, 2);
-  printFloat(gps.course.deg(), gps.course.isValid(), 7, 2);
-  printFloat(gps.speed.kmph(), gps.speed.isValid(), 6, 2);
-  printStr(gps.course.isValid() ? TinyGPSPlus::cardinal(gps.course.value()) : "*** ", 6);
-
-  unsigned long distanceKmToLondon =
-    (unsigned long)TinyGPSPlus::distanceBetween(
-      gps.location.lat(),
-      gps.location.lng(),
-      LONDON_LAT, 
-      LONDON_LON) / 1000;
-  printInt(distanceKmToLondon, gps.location.isValid(), 9);
-
-  double courseToLondon =
-    TinyGPSPlus::courseTo(
-      gps.location.lat(),
-      gps.location.lng(),
-      LONDON_LAT, 
-      LONDON_LON);
-
-  printFloat(courseToLondon, gps.location.isValid(), 7, 2);
-
-  const char *cardinalToLondon = TinyGPSPlus::cardinal(courseToLondon);
-
-  printStr(gps.location.isValid() ? cardinalToLondon : "*** ", 6);
-
-  printInt(gps.charsProcessed(), true, 6);
-  printInt(gps.sentencesWithFix(), true, 10);
-  printInt(gps.failedChecksum(), true, 9);
-  Serial.println();
-  
-  smartDelay(1000);
-
-  if (millis() > 5000 && gps.charsProcessed() < 10){
-    Serial.println(F("No GPS data received: check wiring"));
-  }
-  
-  
-  //delay(10000);
-  // scan for existing networks:
-  Serial.println("** Scan Networks **");
-  byte numSsid = WiFi.scanNetworks();
-  Serial.print("number of available networks:");
-  Serial.println(numSsid);
-  
-  for (int thisNet = 0; thisNet<numSsid; thisNet++) {
-    Serial.print(thisNet);
-    Serial.print(") ");
-    Serial.print(WiFi.SSID(thisNet));
-    Serial.print("\tSignal: ");
-    Serial.print(WiFi.RSSI(thisNet));
-    Serial.print(" dBm");
-    Serial.print("\tEncryption: ");
-    Serial.println(WiFi.encryptionType(thisNet));
-    Serial.print("..");
-    Serial.print("..");
-    Serial.print("Found SSID logging to data.txt...");
-    delay(1000);
-    //myFile.print("SAT COUNT: ");
-    //myFile.println(gps.satellites.value());
-    //myFile.print("LAT: ");
-    //myFile.println(gps.location.lat());
-    //myFile.print("LNG: ");
-    //myFile.println(gps.location.lng());
-    myFile.print("SSID: ");
-    myFile.print(WiFi.SSID(thisNet));
-    myFile.println(".+.");
-    delay(1000);
-    Serial.println("written...");
-    delay(1000);
-    myFile.close();
-    delay(1000);
-  }
-  
-  
-  
-  
-  
-}
-
-
-
-
-
-
-
-
-
-void printMacAddress() {
-  // the MAC address of your Wifi shield
-  byte mac[6];                     
-
-  // print your MAC address:
-  WiFi.macAddress(mac);
-  Serial.print("MAC: ");
-  Serial.print(mac[5],HEX);
-  Serial.print(":");
-  Serial.print(mac[4],HEX);
-  Serial.print(":");
-  Serial.print(mac[3],HEX);
-  Serial.print(":");
-  Serial.print(mac[2],HEX);
-  Serial.print(":");
-  Serial.print(mac[1],HEX);
-  Serial.print(":");
-  Serial.println(mac[0],HEX);
-}
-
-
-
-
-
-// This custom version of delay() ensures that the gps object
-// is being "fed".
-static void smartDelay(unsigned long ms)
-{
   unsigned long start = millis();
-  do 
+  do
   {
     while (ss.available())
       gps.encode(ss.read());
-  } while (millis() - start < ms);
-}
-
-static void printFloat(float val, bool valid, int len, int prec)
-{
-  if (!valid)
-  {
-    while (len-- > 1)
-      Serial.print('*');
-    Serial.print(' ');
+  } while (millis() - start < 1000);
+  byte numSsid = WiFi.scanNetworks();
+  if (ss.available() > 0) {
+    for (int thisNet = 0; thisNet < numSsid; thisNet++) {
+     char* wi = WiFi.SSID(thisNet);
+     if (String(wi) != "") {
+        Serial.print("Dynamic Log: ");
+        Serial.println(String(gps.date.value()));
+        myFile = SD.open(String(gps.date.value()), FILE_WRITE);
+        if (myFile) {
+          myFile.print(" DATE: ");
+          myFile.print(gps.date.value());
+          myFile.print(" TIME: ");
+          myFile.print(gps.time.value());
+          myFile.print(" SAT COUNT: ");
+          myFile.print(gps.satellites.value());
+          myFile.print(" LAT: ");
+          myFile.print(gps.location.lat(), 6);
+          myFile.print(" LNG: ");
+          myFile.print(gps.location.lng(), 6);
+          myFile.print(" MPH: ");
+          myFile.print(gps.speed.mph(), 6);
+          myFile.print(" FEET: ");
+          myFile.print(gps.altitude.feet(), 6);
+          myFile.print(" HEADING: ");
+          myFile.print(gps.course.deg(), 6);
+          myFile.print(" SSID: ");
+          myFile.print(WiFi.SSID(thisNet));
+          myFile.println("");
+          myFile.close();
+        } else {
+          Serial.println("SD card error.");
+        }
+      }
+    }
   }
-  else
-  {
-    Serial.print(val, prec);
-    int vi = abs((int)val);
-    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
-    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
-    for (int i=flen; i<len; ++i)
-      Serial.print(' ');
-  }
-  smartDelay(0);
-}
-
-static void printInt(unsigned long val, bool valid, int len)
-{
-  char sz[32] = "*****************";
-  if (valid)
-    sprintf(sz, "%ld", val);
-  sz[len] = 0;
-  for (int i=strlen(sz); i<len; ++i)
-    sz[i] = ' ';
-  if (len > 0) 
-    sz[len-1] = ' ';
-  Serial.print(sz);
-  smartDelay(0);
-}
-
-static void printDateTime(TinyGPSDate &d, TinyGPSTime &t)
-{
-  if (!d.isValid())
-  {
-    Serial.print(F("********** "));
-  }
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d/%02d/%02d ", d.month(), d.day(), d.year());
-    Serial.print(sz);
-  }
-  
-  if (!t.isValid())
-  {
-    Serial.print(F("******** "));
-  }
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d:%02d:%02d ", t.hour(), t.minute(), t.second());
-    Serial.print(sz);
-  }
-
-  printInt(d.age(), d.isValid(), 5);
-  smartDelay(0);
-}
-
-static void printStr(const char *str, int len)
-{
-  int slen = strlen(str);
-  for (int i=0; i<len; ++i)
-    Serial.print(i<slen ? str[i] : ' ');
-  smartDelay(0);
 }
